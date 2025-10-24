e15b4088fb3eaf42f98a9b29f907b68cb7c2d56b
Junhao Zhang <jerry_mountain@hotmail.com>
2025-10-24 19:15:27 +0000

    Added amazing code

--- src/driver.cc
+++ src/driver.cc
@@ -3,26 +3,29 @@
 #include "room_reservation.hpp"
 
 int main() {
+  // std::cout << "\nAdd all student athletes from file into a hotel with a room
+  // capacity of 2:" << std::endl; RoomReservation
+  // tennis_team("sample/womens_tennis.txt", 2); tennis_team.Print();
 
-  std::cout << "\nAdd all student athletes from file into a hotel with a room capacity of 2:" << std::endl;
-  RoomReservation tennis_team("sample/womens_tennis.txt", 2);
-  tennis_team.Print();
+  // std::cout << "\nRemove an athlete from the hotel:" << std::endl;
+  // tennis_team.RemoveTeamMember("Kida Ferrari");
+  // tennis_team.Print();
 
-  std::cout << "\nRemove an athlete from the hotel:" << std::endl;
-  tennis_team.RemoveTeamMember("Kida Ferrari");
-  tennis_team.Print();
+  // std::cout << "\nRemove another athlete from the hotel, unreserve a room:"
+  // << std::endl; tennis_team.RemoveTeamMember("Rosie Seccia");
+  // tennis_team.Print();
 
-  std::cout << "\nRemove another athlete from the hotel, unreserve a room:" << std::endl;
-  tennis_team.RemoveTeamMember("Rosie Seccia");
-  tennis_team.Print();
+  // std::cout << "\nMove to a new hotel with a room capacity of 5:" <<
+  // std::endl; tennis_team.MoveHotels(5); tennis_team.Print();
 
-  std::cout << "\nMove to a new hotel with a room capacity of 5:" << std::endl;
-  tennis_team.MoveHotels(5);
-  tennis_team.Print();
+  // std::cout << "\nMove back to the original hotel with room capacity 2:" <<
+  // std::endl; tennis_team.MoveHotels(2); tennis_team.Print();
 
-  std::cout << "\nMove back to the original hotel with room capacity 2:" << std::endl;
-  tennis_team.MoveHotels(2);
-  tennis_team.Print();
+  RoomReservation rr("", 3);
+
+  rr.AddTeamMember("Kida Ferrari");
+  rr.RemoveTeamMember("Kida Ferrari");
+  rr.Print();
 
   return 0;
 }
--- src/room_reservation.cc
+++ src/room_reservation.cc
@@ -1,14 +1,104 @@
 #include "room_reservation.hpp"
 
+#include <utility>
 
 void RoomReservation::AddTeamMember(const std::string& student) {
-  // TODO
+  if (rooms_reserved_ == 0 ||
+      rooms_reserved_ * room_capacity_ == athlete_count_) {
+    auto** nr = new std::string*[rooms_reserved_ + 1];
+    if (rooms_ != nullptr) {
+      for (int i = 0; i < rooms_reserved_; i++) {
+        // rooms_[i] is a string*
+        nr[i] = rooms_[i];
+      }
+      delete[] rooms_;
+    }
+
+    nr[rooms_reserved_] = new std::string[room_capacity_];
+    rooms_ = nr;
+    rooms_reserved_++;
+  }
+
+  int oc = GetOccupantCount(rooms_reserved_ - 1);
+  rooms_[rooms_reserved_ - 1][oc] = student;
+  athlete_count_++;
 }
+
 void RoomReservation::RemoveTeamMember(const std::string& student) {
-  // TODO
+  for (int r = 0; r < rooms_reserved_; r++) {
+    for (int c = 0; c < room_capacity_; c++) {
+      if (rooms_[r][c] == student) {
+        rooms_[r][c].clear();
+
+        // first swap the row
+        for (int i = c + 1; i < room_capacity_; i++) {
+          std::swap(rooms_[r][i], rooms_[r][i - 1]);
+        }
+
+        // then swap the rest
+        for (int i = r + 1; i < rooms_reserved_; i++) {
+          // first take the last guy
+          std::swap(rooms_[i - 1][room_capacity_ - 1], rooms_[i][0]);
+          // then do the swap again
+          for (int j = 1; j < room_capacity_; j++) {
+            std::swap(rooms_[i][j], rooms_[i][j - 1]);
+          }
+        }
+
+        bool last_empty = true;
+        for (int i = 0; i < rooms_reserved_; i++) {
+          if (!rooms_[rooms_reserved_ - 1][i].empty()) {
+            last_empty = false;
+            break;
+          }
+        }
+
+        if (last_empty) {
+          delete[] rooms_[rooms_reserved_ - 1];
+          rooms_reserved_--;
+        }
+
+        if (rooms_reserved_ == 0) {
+          delete[] rooms_;
+          rooms_ = nullptr;
+        }
+
+        athlete_count_--;
+
+        return;
+      }
+    }
+  }
+
+  throw std::runtime_error("Student not found");
 }
 void RoomReservation::MoveHotels(int new_room_capacity) {
-  // TODO
+  // reservation count
+  int resc = (int)std::ceil((double)athlete_count_ / new_room_capacity);
+
+  auto** nr = new std::string*[resc];
+
+  int index = 0;
+
+  for (int i = 0; i < resc; i++) {
+    nr[i] = new std::string[new_room_capacity];
+    for (int j = 0; j < new_room_capacity; j++) {
+      int row = index / room_capacity_;
+      int col = index % room_capacity_;
+      nr[i][j] = rooms_[row][col];
+      index++;
+    }
+  }
+
+  // dealloc
+  for (int i = 0; i < rooms_reserved_; i++) {
+    delete[] rooms_[i];
+  }
+  delete[] rooms_;
+
+  rooms_reserved_ = resc;
+  rooms_ = nr;
+  room_capacity_ = new_room_capacity;
 }
 
 // DO NOT MODIFY
@@ -48,7 +138,7 @@ int RoomReservation::GetOccupantCount(int room_number) {
   }
   int occupants = 0;
   for (int i = 0; i < room_capacity_; ++i) {
-    if (room[i] != "") occupants++;
+    if (!room[i].empty()) occupants++;
   }
   return occupants;
 }
